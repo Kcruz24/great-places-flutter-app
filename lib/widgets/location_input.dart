77f125edd3285350de8318459859e959f6adde22
Kevin O. Cruz <kocruz.24@gmail.com>
2022-05-09 08:30:45 -0400

    [308] Allowing Users to Pick a Location on the Map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../helpers/location_helper.dart';
 import '../screens/map_screen.dart';
@@ -25,7 +26,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final LatLng selectedLocation = await Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => MapScreen(
@@ -37,8 +38,9 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
-  }
 
+    print('LOCATION ${selectedLocation.latitude}, ${selectedLocation.longitude}');
+  }
 
   @override
   Widget build(BuildContext context) {
